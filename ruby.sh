bff5b36383b3f4bbf6724749f7c30d0a912b05ee
Aaron Sung <aaronsung@gmail.com>
2015-06-06 23:58:45 +0800

    Added Ruby/Rails short cuts

---
+++ ruby.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#
+# Usage: Add ". ~/ruby.sh" to ~/.profile
+#
+
+export CC=/usr/local/Cellar/apple-gcc42/4.2.1-5666.3/bin/gcc-4.2
+export CXX=/usr/local/Cellar/apple-gcc42/4.2.1-5666.3/bin/g++-4.2
+export CPP=/usr/local/Cellar/apple-gcc42/4.2.1-5666.3/bin/cpp-4.2
+
+alias rgl="rvm gemset list"
+alias rgc="rvm gemset create"
+alias rgu="rvm gemset use"
+
+alias rails_generate="rails generate"
+alias rails_generate_controller="rails generate controller"
+alias rails_generate_migration="rails generate migration"
+alias rails_generate_model="rails generate model"
+alias rails_generate_view="rails generate view"
+
+alias migrate="rake db:migrate"
